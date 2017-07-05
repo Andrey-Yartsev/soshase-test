c6d485557403ea48540f5f643a4fa8d0d9b78291
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2017-07-05 12:59:34 +0300

    add swagger support

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,27 @@
 const Hapi = require('hapi');
+const Inert = require('inert');
+const Vision = require('vision');
+const HapiSwagger = require('hapi-swagger');
 
 const routes = require('./routes/default');
 
 module.exports = async function () {
   let server = new Hapi.Server();
+  const swaggerOptions = {
+    info: {
+      'title': 'Soshase Test',
+      'version': '0.1.0'
+    }
+  };
   server.connection({port: 8000});
   server.route(routes);
-  server.register([],
+  server.register([
+      Inert,
+      Vision,
+      {
+        'register': HapiSwagger,
+        'options': swaggerOptions
+      }],
     (err) => {
       if (err) console.error('Error', err);
       server.start((serverErr) => {
